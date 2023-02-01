dcec49da95960fb5396ab37953a2032ef22315d2
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2023-02-01 16:11:59 +0100

    Fixing console warnings

--- src/components/design/StepperItem.js
+++ src/components/design/StepperItem.js
@@ -6,10 +6,16 @@ function StepperSeparator(){
     )
 }
 
+function StepperValue(props){
+    return(
+        <>{props.children}</>
+    )
+}
+
 export default function StepperItem(props){
 
     let validatedColor = props.validated ? `text-blue-500 dark:text-blue-500` : "";
-    let validatedIcon = props.validated ? <svg aria-hidden="true" className="w-4 h-4 mr-2 sm:w-5 sm:h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg> : <span className="mr-2">{props.number}</span>
+    let validatedIcon = props.validated ? <svg aria-hidden="true" className="w-4 h-4 mr-2 sm:w-5 sm:h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg> : <span className="mr-2">{props.number}</span>
     let lastLi = props.isLast ? "" : `md:w-full after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700`;
     let lastSpan = props.isLast ? "" : `after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500`;
     
@@ -17,7 +23,7 @@ export default function StepperItem(props){
 
     let separatedText = props.name.split(" ").map(function(value) {
         return (
-            <>{value}<StepperSeparator/></>  
+            <StepperValue key={value}>{value}<StepperSeparator/></StepperValue>  
         )
     })
 
