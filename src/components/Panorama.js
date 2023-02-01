4ef8085fd9770077746ab02e014281c0ef9f9682
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2023-02-01 17:21:57 +0100

    Adding JSON export

--- src/components/Panorama.js
+++ src/components/Panorama.js
@@ -199,6 +199,15 @@ function convertToJSON(panImage){
         onRender: v.onRender
     };
 
+    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+        JSON.stringify(jsonConfig)
+    )}`;
+    const link = document.createElement("a");
+    link.href = jsonString;
+    link.download = "data.json";
+
+    link.click();
+
     return jsonConfig;
 }
 
