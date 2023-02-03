9e003bcdd5ed2993ce53274d8fc4d4e8e81a9d1a
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2023-02-03 14:36:01 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      animation: {
-        'text': 'text 5s ease infinite',
-        'waving-hand': 'wave 2s linear infinite'
-      },
       keyframes: {
         text: {
           '0%, 100%': {
@@ -19,16 +15,18 @@ module.exports = {
             'background-size': '200% 200%',
             'background-position': 'right center',
           },
-        }, wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
-        }
+        }, headShake: {
+          '0%': { transform: 'translateX(0)' },
+          '6.5%': { transform: 'translateX(-6px) rotateY(-9deg)' },
+          '18.5%': { transform: 'translateX(5px) rotateY(7deg)' },
+          '31.5%': { transform: 'translateX(-3px) rotateY(-5deg)' },
+          '43.5%': { transform: 'translateX(2px) rotateY(3deg)' },
+          '50%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        text: 'text 5s ease infinite',
+        headShake: 'headShake 2s infinite',
       },
     },
   },
