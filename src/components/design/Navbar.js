93cad59fb65315e438d4ecec59a08ca4e5ff0934
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2023-02-03 14:34:13 +0100

    Adding Router

--- src/components/design/Navbar.js
+++ src/components/design/Navbar.js
@@ -4,51 +4,48 @@ import Logo from "./Logo";
 import Row from "./Row";
 import Button from "./Button";
 
+import Home from '../../pages/Home'
+import Gallery from '../../pages/Gallery'
+import Favorites from '../../pages/Favorites'
+import Help from '../../pages/Help'
+import Edit from '../../pages/Edit'
+
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+
 export default function Navbar(props){
     return(
-        <Row>
-            <Logo/>
+        <Router>
+            <Row>
+                <Logo></Logo>
+        
+                <Button variant="classic" size="small">
+                    <Link to="/">Home</Link>
+                </Button>
+
+                <Button variant="classic" size="small">
+                    <Link to="/gallery">Gallery</Link>
+                </Button>
+
+                <Button variant="classic">
+                    <Link to="/favorites">My favorites</Link>
+                </Button>
+
+                <Button variant="classic" size="small">
+                    <Link to="/help">Help</Link>
+                </Button>
+
+                <Button round="medium" size="normal">
+                    <Link to="/edit">Get started</Link>
+                </Button>  
+            </Row>
             
-            <Button className="basis-1/12 link link-underline link-underline-black"
-                text="Home"
-                noHover
-                noBackground
-                function={() => {
-                alert("WIP - In Progress");
-                }}
-            />
-
-            <Button className="basis-1/12 link link-underline link-underline-black"
-            text="Gallery"
-            noHover noBackground
-            function={() => {
-                alert("WIP - In Progress");
-            }}
-            />           
-            
-            <Button className="basis-1/12 link link-underline link-underline-black"
-            text="My favorites"
-            noHover noBackground
-            function={() => {
-                alert("WIP - In Progress");
-            }}
-            />
-
-            <Button className="basis-1/12 link link-underline link-underline-black"
-            text="Help"
-            noHover noBackground
-            function={() => {
-                alert("WIP - In Progress");
-            }}
-            />                  
-
-            <Button className="basis-1/12"
-                text="Sign up"
-                noHover
-                function={() => {
-                alert("WIP - In Progress");
-            }}
-            />      
-        </Row>
+            <Routes>
+                <Route exact path='/' element={<Home/>}></Route>
+                <Route exact path='/gallery' element={< Gallery />}></Route>
+                <Route exact path='/favorites' element={< Favorites />}></Route>
+                <Route exact path='/help' element={< Help />}></Route>
+                <Route exact path='/edit' element={< Edit />}></Route>
+            </Routes>
+        </Router>       
     )
 }
