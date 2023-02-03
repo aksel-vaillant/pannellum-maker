aa23a35fa2b7eae4c273aff1bb0d124cd4bcba08
aksel-vaillant <82941071+aksel-vaillant@users.noreply.github.com>
2023-02-03 14:34:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,53 +2,18 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 import "./index.css";
-import Panorama from "./components/Panorama";
-import { StepperControl, StepperItem, StepperList, Navbar} from "./components/design";
-
 import reportWebVitals from './reportWebVitals';
 
-function StepperPanel(props) {
-  if (props.value === 0) {
-      return (<div className="h-96" style={{ color: 'red' }}>
-          <h1>Pick your scene stuff</h1>
-      </div>)
-  }
-  else {
-      if (props.value === 1) {
-          return (<Panorama src="test2.jpeg"/>)
-
-      }
-      else {
-          return (<div className="h-96" style={{ color: 'orange' }}>
-              <h1>Good job! Now, share it with your friends or directly on your website</h1>
-          </div>)
-      }
-  }
-}
+import { Navbar } from "./components/design";
+import Home from "./pages/Home"
 
 export default function App() {
-  const [step, setStep] = React.useState(0)
-  const onChange = nextStep => {
-      setStep(nextStep < 0 ? 0 : nextStep > 2 ? 2 : nextStep);
-  };
-
-  const onNext = () => onChange(step + 1);
-  const onPrevious = () => onChange(step - 1);
+  
 
   return (
       <>
         <div className="w-10/12 mx-auto">
-          <Navbar/>
-
-          <StepperList>
-              <StepperItem number="1" name="Pick your scene" validated={step === 1 || step === 2} current={step === 0}></StepperItem>
-              <StepperItem number="2" name="Add information" validated={step === 2} current={step === 1}></StepperItem>
-              <StepperItem number="3" name="Save and share" current={step === 2} isLast></StepperItem>
-          </StepperList>
-
-          <StepperPanel value={step}></StepperPanel>
-
-          <StepperControl value={step} min={0} max={2} onPrevious={onPrevious} onNext={onNext} />
+            <Navbar/>
         </div>        
       </>
     );
