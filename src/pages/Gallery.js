4b45e5502e403d62fb6c9ddd6882c5c3189970c1
Clément Le Batard <clement.lebatard1@gmail.com>
2023-02-10 13:29:14 +0100

    Changes on gallery page
    - Added search bar
    - Added filter on search bar
    - Added a dropdown menu
    - Added filters (A to Z / Z to A / My favorites)on dropdown menu
    - Problem on filter button that cannot find src in photos array

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,102 +1,162 @@
 import React from "react";
 import Gallery from "react-photo-gallery";
+import { useState, useEffect } from "react";
+import dropdownMenu from "../components/design/dropdownMenu";
+import { Row } from "../components/design";
+import Example from "../components/design/dropdownMenu";
+
 
 export default function PanGallery(props){
-    const photos = [
-        {
-          src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-          width: 1,
-          height: 1
-        },
-        {
-          src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-          width: 3,
-          height: 4
-        },
-        {
-          src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-          width: 3,
-          height: 4
-        },
-        {
-          src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-          width: 3,
-          height: 4
-        },
-        {
-          src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-          width: 3,
-          height: 4
-        },
-        {
-          src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
-          width: 3,
-          height: 4
-        },
-        {
-          src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/u9cG4cuJ6bU/4927x1000",
-          width: 4927,
-          height: 1000
-        },
-        {
-          src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/pF1ug8ysTtY/600x400",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/A-fubu9QJxE/800x533",
-          width: 4,
-          height: 3
-        },
-        {
-          src: "https://source.unsplash.com/5P91SF0zNsI/740x494",
-          width: 4,
-          height: 3
-        }
-    ];
+  const photos = [
+    {
+      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+      width: 4,
+      height: 3,
+      favorite : "true"
+    },
+    {
+      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/u9cG4cuJ6bU/4927x1000",
+      width: 4927,
+      height: 1000
+    },
+    {
+      src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/pF1ug8ysTtY/600x400",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/A-fubu9QJxE/800x533",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "https://source.unsplash.com/5P91SF0zNsI/740x494",
+      width: 4,
+      height: 3
+    }
+];
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [userAction, setUserAction] = useState("");
+
+
+    const compareZToA = (a, b) => (a.src < b.src ? 1 : -1);
+    const compareAToZ = (a, b) => (a.src > b.src ? 1 : -1);
+    const compareFav = (a) => (a.favorite ? true : false);
+
+    let sort = [
+      {
+          id : 0,
+          name : "A - Z",
+          function :  compareAToZ
+      },{
+          id : 1,
+          name : "Z - A",
+          function :  compareZToA
+      },{
+          id : 2,
+          name : "My favorites",
+          function : compareFav
+      }
+  ];
+
+    let photoUpdate = [];
+
+    const handleSearchTerm = (e) => {
+        console.log(e.target.value);
+        let value = e.target.value;
+        setSearchTerm(value);
+    };
+
+    const getData = (data) =>{
+      console.log(data);
+      console.log(photos.filter((photos) =>{return photos.favorite}));
+      //setUserAction(data);
+    }
+
+
+    console.log(searchTerm);
       
     return(
         <>
-            <h1 className="text-2xl">Gallery</h1> <hr className="border-black my-5"/>            
-            
-            <Gallery photos={photos} direction={"column"} margin={10}></Gallery>
+            <h1 className="text-2xl">Gallery</h1> <hr className="border-black my-5"/>
+            <Row>
+              <Example onSubmit={getData} sort={sort} valueChangeCallback={getData}></Example>
+              <div className="flex w-72">
+                <input className="inline-flex w-72 items-center content-center rounded pr-2.5 h-35 text-base leading-none text-black shadow-lg" type="text" name="searchBar" id="serachBar" placeholder="Recherchez..." onChange={handleSearchTerm}></input>  
+              </div>
 
-            <p>Ooooh</p>
+              <div className="searchResults">
+                {photos
+                  .filter((photos) =>{
+                    return photos.src.includes(searchTerm);
+                  }).sort(()=>{return getData})
+                  .map((photos) => {
+                    photoUpdate.push(photos);
+                  })}
+              </div>        
+            </Row>
+            {photoUpdate.length > 0 ? (<Gallery photos={photoUpdate} direction={"column"} margin={10}></Gallery>) : (<p>There's no files with this name</p>)}           
         </>
     )
 }
